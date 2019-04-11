4683dc1a3730ab7028757da719e729dd9b3c490d
zacateras <mic.sitko@gmail.com>
2019-04-11 21:03:51 +0200

    Exposing tensorboard via ngrok.

---
+++ tools/ngrok.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! -d "tmp" ]; then mkdir "tmp"; fi
+
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -P 'tmp'
+unzip -o tmp/ngrok-stable-linux-amd64.zip -d tmp
+rm tmp/ngrok-stable-linux-amd64.zip
+
+tensorboard --logdir ${1:-tmp/out} --host 0.0.0.0 --port 6006 &
+
+if [ ! -z $2 ]; then ./tmp/ngrok authtoken $2; fi
+./tmp/ngrok http 6006 &
+
+curl -s http://localhost:4040/api/tunnels
